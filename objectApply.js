4c33f88898fd84a3e4243ebbbccf0787c1e40c9a
mdmahbubulalam <alam.diu13@gmail.com>
2021-02-06 12:51:26 +0600

    object call from another object

--- objectApply.js
+++ objectApply.js
@@ -24,10 +24,11 @@ const friendlyPerson = {
     salary: 35000
 }
 
-/* ekhane normalPerson er chargeBill method ta call korlam heroPerson object a.ekhane this er value ta heroPerson hobe.
-   normalPerson er chargeBill method a jotogulo argument thakbe totota argument pathate hobe. */
-normalPerson.chargeBill.call(heroPerson,900,300,30);
-normalPerson.chargeBill.call(friendlyPerson,3000,300,30);
+/* ekhane normalPerson er chargeBill method ta apply korlam heroPerson object a.ekhane this er value ta heroPerson hobe.
+   call and apply duita diyei jekono objecte er method ke direct call kora jay, but apply er khetre argument ke array hisabe 
+   pathate hobe.  */
+normalPerson.chargeBill.apply(heroPerson, [900,300,30]);
+normalPerson.chargeBill.apply(friendlyPerson,[3000,300,30]);
 console.log(heroPerson.salary);
 console.log(friendlyPerson.salary);
 console.log(normalPerson.salary);
